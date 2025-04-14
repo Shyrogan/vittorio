be7e6d453f69f8c08c087d0ce6d60092383f4e3a
Sébastien <sebastien@shyrogan.fr>
2025-04-14 15:28:01 +0200

    fix attempt

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,19 +2,20 @@ import axios from "axios";
 import { load } from "cheerio";
 
 /**
+ * @param {Function} fetch - Custom fetch implementation
  * @param {{ brand: string, url: string }} brand 
  **/
-export async function scrape(brand) {
+export async function scrape(fetch, brand) {
   if (!brand) return undefined
   try {
-    const response = await axios.get(brand.url, {
+    const response = await fetch(brand.url, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
       },
       timeout: 10000
     });
 
-    const $ = load(response.data);
+    const $ = load(await response.text());
     const contactBlock = $('html > body.skin-green.body-catalogo > div.container > section.content > div.row > div.col-md-4 > div.box > div.box-body');
 
     // Extract social media links
--- src/routes/[id]/+page.server.js
+++ src/routes/[id]/+page.server.js
@@ -4,7 +4,7 @@ import json from "../../../static/brands.json"
 /** @type {import('./$types').PageServerLoad} */
 export const load = async ({ params, fetch }) => {
   const { id } = params
-  const result = await scrape(json[Number(id)])
+  let result = await scrape(fetch, json[Number(id)])
 
   return {
     brand: json[Number(id)],
